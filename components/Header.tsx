201ea47fe12d358c6b8f26bf8c35bce10d12ded1
facuariasla <facundolautaroarias@hotmail.com>
2022-07-05 23:42:50 -0300

    first block finished

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,12 +7,16 @@ import hd from "../public/assets/hd-icon.svg";
 import eye from "../public/assets/eye-icon.svg";
 import fourk from "../public/assets/4k-icon.svg";
 import warn from "../public/assets/warn-icon.svg";
+import bicon from "../public/assets/b-icon.svg";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-        <div >
-          <nav>basement</nav>
+        <div>
+          <a href="https://basement.studio/" target='_blank' className={styles.baselogo}>basement</a>
+          <a href="https://basement.studio/" target='_blank' className={styles.b_icon}>
+            <Image src={bicon} height={60} width={60}/>
+          </a>
         </div>
         <nav className={styles.icons}>
           <Image src={circle} />
--- components/HomeTitle.tsx
+++ components/HomeTitle.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Header from "./Header";
 
 import styles from "../styles/Layout.module.scss";
 
@@ -7,9 +7,17 @@ const HomeTitle = () => {
   return (
     <div className={styles.titles_container}>
       <h1 className={styles.home_title}>BASEMENT</h1>
-      <h1 className={styles.home_title2}>SUPLY</h1>
+      <div className={styles.title2_container}>
+        <div>
+          <h4>EST</h4>
+        </div>
+        <h1 className={styles.home_title2}>SUPLY</h1>
+        <div>
+          <h4>2K22</h4>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeTitle
+export default HomeTitle;
