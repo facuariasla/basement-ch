f1d6b373202d266f29aa1e0567d330e80da44e04
facuariasla <facundolautaroarias@hotmail.com>
2022-07-06 14:01:25 -0300

    Layout module all nested

--- components/HomeTitle.tsx
+++ components/HomeTitle.tsx
@@ -7,11 +7,14 @@ const HomeTitle = () => {
   return (
     <div className={styles.titles_container}>
       <h1 className={styles.home_title}>BASEMENT</h1>
+
       <div className={styles.title2_container}>
         <div>
           <h4>EST</h4>
         </div>
+        
         <h1 className={styles.home_title2}>SUPLY</h1>
+
         <div>
           <h4>2K22</h4>
         </div>
