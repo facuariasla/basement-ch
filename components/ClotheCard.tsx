e38677e2417d1477cf7df3a976000455496b3df8
facuariasla <facundolautaroarias@hotmail.com>
2022-07-06 11:56:21 -0300

    clothes added

--- components/ClotheCard.tsx
+++ components/ClotheCard.tsx
@@ -1,19 +1,24 @@
 import React from 'react'
-import styles from '../styles/HomeClothes.module.scss'
-import hoodie from '../public/assets/hoodie.png'
 import Image from 'next/image'
+import styles from '../styles/HomeClothes.module.scss'
+import hoodie from '../public/assets/hoodie.png';
+import shirt from '../public/assets/shirt.png'
+import cap from '../public/assets/cap.png'
+
 // Datos de la ropa van como props
 
-const ClotheCard = () => {
+const ClotheCard = ({ productData }:any) => {
   return (
-    <div className={styles.clothe_card}>
-      <div className={styles.img_cont}>
+    <div key={productData.id} className={styles.clothe_card}>
+      <div className={styles.img_background}>
+        <div className={styles.img_container}>
         {/* arreglar configs de la imagen */}
-        <Image src={hoodie} width={1050}/>
+          <Image src={productData.src} width={900} objet-fit='cover' />
+        </div>
       </div>
       <div className={styles.description}>
-        <p>clothename</p>
-        <p> $0.00</p>
+        <p>{productData.clothename}</p>
+        <p> ${productData.price}</p>
       </div>
     </div>
   )
--- components/Clothes.tsx
+++ components/Clothes.tsx
@@ -1,15 +1,39 @@
-import React from 'react'
-import ClotheCard from './ClotheCard'
-import styles from '../styles/HomeClothes.module.scss'
+import React from "react";
+import ClotheCard from "./ClotheCard";
+import styles from "../styles/HomeClothes.module.scss";
+import hoodie from "../public/assets/hoodie.png";
+import shirt from "../public/assets/shirt.png";
+import cap from "../public/assets/cap.png";
+
+const clothesData = [
+  {
+    id: Math.random() * 9999,
+    clothename: "Black t-shirt",
+    price: "7.95",
+    src: shirt,
+  },
+  {
+    id: Math.random() * 9999,
+    clothename: "Black hoodie",
+    price: "13",
+    src: hoodie,
+  },
+  {
+    id: Math.random() * 9999,
+    clothename: "Black cap",
+    price: "23",
+    src: cap,
+  },
+];
 
 const Clothes = () => {
   return (
     <div className={styles.clothes_container}>
-      <ClotheCard/>
-      <ClotheCard/>
-      <ClotheCard/>
+      {clothesData?.map((el) => (
+        <ClotheCard productData={el} key={el.id}/>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Clothes
+export default Clothes;
