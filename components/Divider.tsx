3a7bdd612718097f01fdd11367495c7957a53b09
facuariasla <facundolautaroarias@hotmail.com>
2022-07-06 14:18:24 -0300

    moving divider text keyframes

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -5,7 +5,10 @@ const Divider = () => {
   return (
     <div className={styles.divider}>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro placeat magnam maxime, illo ab minima? Perspiciatis praesentium cum, corrupti repellendus culpa provident impedit ratione facilis voluptates iure, voluptatibus maxime modi!
+        A man can't have enough basement swag —
+        A man can't have enough basement swag —
+        A man can't have enough basement swag —
+        A man can't have enough basement swag —
       </p>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,10 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import HomeTitle from "../components/HomeTitle";
 import Layout from "../components/Layout";
-import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Layout>
         <Header />
         <HomeTitle />
