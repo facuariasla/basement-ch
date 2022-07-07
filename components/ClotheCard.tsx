f4f5ed260dfe4c161190c92775d4114a1507bcea
facuariasla <facundolautaroarias@hotmail.com>
2022-07-07 17:32:08 -0300

    cart created

--- components/ClotheCard.tsx
+++ components/ClotheCard.tsx
@@ -1,20 +1,44 @@
-import React from 'react'
-import Image from 'next/image'
-import styles from '../styles/HomeClothes.module.scss'
-import addtocart from '../public/assets/addtocart.svg'
-
+import React, { useState } from "react";
+import Image from "next/image";
+import styles from "../styles/HomeClothes.module.scss";
+import addtocart from "../public/assets/addtocart.svg";
+import { useRecoilState } from "recoil";
+import { cartState } from "../utils/recoil";
 
 // Datos de la ropa van como props
 
-const ClotheCard = ({ productData }:any) => {
+const ClotheCard = ({ productData }: any) => {
+  const [cart, setCart] = useRecoilState(cartState);
+  const [items, setItems] = useState<any>([]);
+
+  const itemAdded = (el: any) => {
+    console.log("productId ", el.target.alt);
+    setItems([...items, el.target.alt]);
+    console.log(items);
+  };
+
   return (
     <div key={productData.id} className={styles.clothe_card}>
-      <div className={styles.img_background}>
+      <div
+        className={styles.img_background}
+      >
         <div className={styles.img_container}>
-          <Image src={productData.src} width={900} objet-fit='cover' />
+          <Image
+            src={productData.src}
+            width={900}
+            objet-fit="cover"
+            alt={productData.productId}
+            onClick={(e) => itemAdded(e)}
+          />
         </div>
         <div className={styles.addtocart}>
-          <Image src={addtocart} width={200} objet-fit='cover' />
+          <Image
+            src={addtocart}
+            width={200}
+            objet-fit="cover"
+            alt={productData.productId}
+            onClick={(productData) => itemAdded(productData)}
+          />
         </div>
       </div>
       <div className={styles.description}>
@@ -22,7 +46,7 @@ const ClotheCard = ({ productData }:any) => {
         <p> ${productData.price}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ClotheCard
+export default ClotheCard;
--- components/Clothes.tsx
+++ components/Clothes.tsx
@@ -4,35 +4,49 @@ import styles from "../styles/HomeClothes.module.scss";
 import hoodie from "../public/assets/hoodie.png";
 import shirt from "../public/assets/shirt.png";
 import cap from "../public/assets/cap.png";
+import { useRecoilState } from "recoil";
+import { productsState } from "../utils/recoil";
+import { cartState } from "../utils/recoil";
 
 const clothesData = [
   {
     id: Math.random() * 9999,
+    productId: 1,
     clothename: "Black t-shirt",
     price: "7.95",
     src: shirt,
     description:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint doloremque illum reprehenderit vitae dolore.",
+      stock: 10,
   },
   {
     id: Math.random() * 9999,
+    productId: 2,
     clothename: "Black hoodie",
     price: "13",
     src: hoodie,
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat cupiditate laboriosam laborum.",
+      stock: 12,
   },
   {
     id: Math.random() * 9999,
+    productId: 3,
     clothename: "Black cap",
     price: "23",
     src: cap,
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni iste ex ducimus assumenda.",
+      stock: 8,
   },
 ];
 
 const Clothes = () => {
+  const [products, setProducts] = useRecoilState(productsState);
+  const [cart, setCart] = useRecoilState(cartState);
+
+  
+
   return (
     <div className={styles.clothes}>
       <div className={styles.clothes_container}>
